4d0d9af1a94eee3ac6dc980937d8e1bc91da90cf
Mariia Gushuley <maryshka.moroz@gmail.com>
2017-02-17 13:29:31 +0200

    changed inspection and added Math.abs

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var userNumber = +prompt('Enter your number for calculation power');
 var userPower = +prompt('Enter your power');
 
-if ( (typeof(userNumber) === 'number') && (isNaN(userNumber) === false) && (typeof(userPower) === 'number')  && (Number.isInteger(userPower) == false)) {
+if ( (typeof(userNumber) === 'number') && (typeof(userPower) === 'number')  && (isNaN(userNumber) === false) && (Number.isInteger(userPower) == true)) {
     console.log('Result is', pow(userNumber, userPower) );
 } else {
 	console.log('Error: the number and power shoud be number');
@@ -20,13 +20,14 @@ var result = number;
  		return 1;
  	} 
 
-	for (var i = 1; i < power; i++) {
+	for (var i = 1; i < Math.abs(power); i++) {
  	   result *= number;
 	}
  	
  	if (power > 0) {
  		return result;
  	} else {
+ 		
  		return 1 / result;
  	}
  	
